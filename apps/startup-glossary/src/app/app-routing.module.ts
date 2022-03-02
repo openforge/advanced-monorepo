a3b2c0a9a5295cc2421c736871a05b9f8df636f9
Fernando del olmo <fer.olmo92@gmail.com>
2022-03-02 09:52:27 +0100

    feat(apps): add initial angular app

--- apps/startup-glossary/src/app/app-routing.module.ts
+++ apps/startup-glossary/src/app/app-routing.module.ts
@@ -4,7 +4,11 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
     {
         path: 'home',
-        loadChildren: () => import('./home/home.module').then(m => m.HomePageModule),
+        loadChildren: () => import('@mobile-academy/features/glossary-home-module').then(m => m.GlossaryHomeModule),
+    },
+    {
+        path: 'category',
+        loadChildren: () => import('@mobile-academy/features/glossary-category-module').then(m => m.GlossaryCategoryModule),
     },
     {
         path: '',
--- apps/startup-glossary/src/app/app.module.ts
+++ apps/startup-glossary/src/app/app.module.ts
@@ -3,15 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
+import { CategoryService } from '@mobile-academy/data-access/services';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
 @NgModule({
     declarations: [AppComponent],
     entryComponents: [],
     imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-    providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+    providers: [
+        CategoryService,
+        {
+            provide: RouteReuseStrategy,
+            useClass: IonicRouteStrategy,
+        },
+    ],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
