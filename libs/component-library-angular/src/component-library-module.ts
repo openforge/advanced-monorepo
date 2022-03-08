dff16f71bc2662d77df99ea2b54910786ec27ea6
Fernando del olmo <fer.olmo92@gmail.com>
2022-03-08 09:28:30 +0100

    feat(): cleanup non used components

--- libs/component-library-angular/src/component-library-module.ts
+++ libs/component-library-angular/src/component-library-module.ts
@@ -1,20 +1,13 @@
 import { NgModule } from '@angular/core';
 import { defineCustomElements } from 'component-library/loader';
 
-import { MyButton, MyCheckbox, MyComponent, MyInput, MyPopover, MyRadio, MyRadioGroup, MyRange } from './directives/proxies';
+import { MyComponent } from './directives/proxies';
 
 defineCustomElements(window);
 
 const DECLARATIONS = [
     // proxies
     MyComponent,
-    MyButton,
-    MyCheckbox,
-    MyInput,
-    MyPopover,
-    MyRadio,
-    MyRadioGroup,
-    MyRange,
 ];
 
 @NgModule({
