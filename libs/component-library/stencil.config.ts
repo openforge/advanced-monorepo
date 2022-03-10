d5a6ae21be1d9a21904e6553e7e0a353f0627b24
Fernando del olmo <fer.olmo92@gmail.com>
2022-03-10 15:39:01 +0100

    feat(): cleanup vue

--- libs/component-library/stencil.config.ts
+++ libs/component-library/stencil.config.ts
@@ -1,6 +1,5 @@
 import { angularOutputTarget } from '@stencil/angular-output-target';
 import { Config } from '@stencil/core';
-import { vueOutputTarget } from '@stencil/vue-output-target';
 
 export const config: Config = {
     namespace: 'component-library',
@@ -10,10 +9,6 @@ export const config: Config = {
             componentCorePackage: 'component-library',
             directivesProxyFile: '../component-library-angular/src/directives/proxies.ts',
             includeImportCustomElements: true,
-        }),
-        vueOutputTarget({
-            componentCorePackage: 'component-library',
-            proxiesFile: '../component-library-vue/src/proxies.ts',
         }),
         {
             type: 'dist-custom-elements',
